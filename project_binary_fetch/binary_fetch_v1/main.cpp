49442125c44255d48cdbaceedb13e92ec57d69f5
BinaryOxide <234583143+BinaryOxide@users.noreply.github.com>
2026-01-08 23:58:53 +0600

    ui: set default OS info colors to white

--- project_binary_fetch/binary_fetch_v1/main.cpp
+++ project_binary_fetch/binary_fetch_v1/main.cpp
@@ -1268,8 +1268,6 @@ int main(){
         }
         // end of the detailed network section////////////////////////////////////////////////
 
-
-
         // OS Info (JSON Driven)
         if (isEnabled("os_info")) {
             lp.push("");
@@ -1277,9 +1275,9 @@ int main(){
             // Header
             if (isSubEnabled("os_info", "show_header")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "#-", "blue") << "#- " << r
-                    << getColor("os_info", "header_text_color", "green") << "Operating System " << r
-                    << getColor("os_info", "separator_line", "blue")
+                ss << getColor("os_info", "#-", "white") << "#- " << r
+                    << getColor("os_info", "header_text_color", "white") << "Operating System " << r
+                    << getColor("os_info", "separator_line", "white")
                     << "-----------------------------------------------#" << r;
                 lp.push(ss.str());
             }
@@ -1287,60 +1285,60 @@ int main(){
             // Name
             if (isSubEnabled("os_info", "show_name")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Name                      " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "name_value_color", "yellow") << os.GetOSName() << r;
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Name                      " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "name_value_color", "white") << os.GetOSName() << r;
                 lp.push(ss.str());
             }
 
             // Build
             if (isSubEnabled("os_info", "show_build")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Build                     " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "build_value_color", "yellow") << os.GetOSVersion() << r;
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Build                     " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "build_value_color", "white") << os.GetOSVersion() << r;
                 lp.push(ss.str());
             }
 
             // Architecture
             if (isSubEnabled("os_info", "show_architecture")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Architecture              " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "arch_value_color", "cyan") << os.GetOSArchitecture() << r;
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Architecture              " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "arch_value_color", "white") << os.GetOSArchitecture() << r;
                 lp.push(ss.str());
             }
 
             // Kernel
             if (isSubEnabled("os_info", "show_kernel")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Kernel                    " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "kernel_value_color", "cyan") << os.get_os_kernel_info() << r;
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Kernel                    " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "kernel_value_color", "white") << os.get_os_kernel_info() << r;
                 lp.push(ss.str());
             }
 
             // Uptime
             if (isSubEnabled("os_info", "show_uptime")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Uptime                    " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "uptime_value_color", "magenta") << os.get_os_uptime() << r;
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Uptime                    " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "uptime_value_color", "white") << os.get_os_uptime() << r;
                 lp.push(ss.str());
             }
 
             // Install Date
             if (isSubEnabled("os_info", "show_install_date")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Install Date              " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "install_date_value_color", "magenta")
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Install Date              " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "install_date_value_color", "white")
                     << os.get_os_install_date() << r;
                 lp.push(ss.str());
             }
@@ -1348,16 +1346,18 @@ int main(){
             // Serial
             if (isSubEnabled("os_info", "show_serial")) {
                 std::ostringstream ss;
-                ss << getColor("os_info", "~", "blue") << "~ " << r
-                    << getColor("os_info", "label_color", "green") << "Serial                    " << r
-                    << getColor("os_info", ":", "blue") << ": " << r
-                    << getColor("os_info", "serial_value_color", "yellow")
+                ss << getColor("os_info", "~", "white") << "~ " << r
+                    << getColor("os_info", "label_color", "white") << "Serial                    " << r
+                    << getColor("os_info", ":", "white") << ": " << r
+                    << getColor("os_info", "serial_value_color", "white")
                     << os.get_os_serial_number() << r;
                 lp.push(ss.str());
             }
         }
 
-		//end of the OS info section////////////////////////////////////////////////
+        //end of the OS info section////////////////////////////////////////////////
+
+
 
         // CPU Info (JSON Driven)
         if (isEnabled("cpu_info")) {
